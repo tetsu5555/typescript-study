6090702d7f94e00c7d91bbbb9f713f0b8e2989ad
tetsu5555 <tetete1118@gmail.com>
2020-02-11 12:29:38 +0900

    feat: express server

--- ts-express/src/server/index.ts
+++ ts-express/src/server/index.ts
@@ -1,1 +1,15 @@
-console.log('Hello World')
+import Express from 'express'
+
+const app = Express()
+
+app.get('/', (req, res) => {
+  const data = { message: 'pong' }
+  res.send(data)
+})
+
+const port = 8888
+const host = 'localhost'
+
+app.listen(port, host, () => {
+  console.log(`Running on http://${host}:${port}`)
+})
