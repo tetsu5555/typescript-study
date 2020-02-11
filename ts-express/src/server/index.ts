3a1e48eddcd9a24b48c8cea42f50b7d107b09b51
tetsu5555 <tetete1118@gmail.com>
2020-02-11 12:41:39 +0900

    feat: server

--- ts-express/src/server/index.ts
+++ ts-express/src/server/index.ts
@@ -1,11 +1,28 @@
 import Express from 'express'
+import cors from 'cors'
 
 const app = Express()
+app.use(cors())
 
-app.get('/', (req, res) => {
-  const data = { message: 'pong' }
-  res.send(data)
+app.get('/api/health', (req, res) => {
+  res.send({ message: 'pong' })
 })
+
+app.use((req, res, next) => {
+  res.sendStatus(404)
+  next({ statusCode: 404 })
+})
+
+app.use(
+  (
+    err: { statusCode: number },
+    req: Express.Request,
+    res: Express.Response,
+    next: Express.NextFunction
+  ) => {
+    console.log(err.statusCode)
+  }
+)
 
 const port = 8888
 const host = 'localhost'
