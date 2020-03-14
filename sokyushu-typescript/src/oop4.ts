da0f3f9d5fb8fcc4e442118e034811628c59a497
tetsu5555 <tetete1118@gmail.com>
2020-03-15 08:25:08 +0900

    Person4

---
+++ sokyushu-typescript/src/oop4.ts
@@ -0,0 +1,19 @@
+class Person4 {
+  private _age: number;
+
+  get age(): number {
+    return this._age;
+  }
+
+  set age(value: number) {
+    if (value < 0) {
+      throw new RangeError('ageプロパティは整数で指定してください');
+    }
+    this._age = value;
+  }
+}
+
+
+let p4 = new Person4();
+p4.age = 10
+console.log(p4.age)
