ea7542c3932025da2b929ac02c70b2a2e2effa5e
tetsu5555 <tetete1118@gmail.com>
2020-03-15 07:54:24 +0900

    easy way to write access modifier

---
+++ sokyushu-typescript/src/oop3.ts
@@ -0,0 +1,18 @@
+class Person3 {
+  // コンストラクタの引数にアクセス修飾子を付与することで
+  // プロパティの定義から代入までのコードを代用できる
+  constructor(private name: string, private sex: string) {
+    this.name = name;
+    this.sex = sex;
+  }
+
+  public show(): string {
+    return `${this.name}は${this.sex}です。`;
+  }
+}
+
+let p3 = new  Person3('tetsu', '男');
+console.log(p2.show())
+
+// ↓ privateなためコンパイルエラーになる
+// console.log(p2.name)
