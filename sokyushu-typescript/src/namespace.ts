2cd838fb6da4d28513a76bce8adfb642e1da85af
tetsu5555 <tetete1118@gmail.com>
2020-03-15 14:27:03 +0900

    namespace

---
+++ sokyushu-typescript/src/namespace.ts
@@ -0,0 +1,13 @@
+// 名前空間を定義
+namespace MainApp {
+  // exportを指定しないと参照できなかった
+  export class Hoge {
+    hoge() {
+      console.log("hoge!!!");
+    }
+  }
+}
+
+// MainApp名前空間に定義されたHogeクラスを参照する
+const hoge = new MainApp.Hoge();
+hoge.hoge()
