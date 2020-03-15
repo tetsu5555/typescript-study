2c2a2a2df91a1afb043f587fb9b7a26722b1c19f
tetsu5555 <tetete1118@gmail.com>
2020-03-15 14:28:09 +0900

    update

--- sokyushu-typescript/src/namespace.ts
+++ sokyushu-typescript/src/namespace.ts
@@ -6,8 +6,13 @@ namespace MainApp {
       console.log("hoge!!!");
     }
   }
+
+  export function foo() {
+    console.log("foo!!!");
+  }
 }
 
 // MainApp名前空間に定義されたHogeクラスを参照する
 const hoge = new MainApp.Hoge();
-hoge.hoge()
+hoge.hoge();
+MainApp.foo();
